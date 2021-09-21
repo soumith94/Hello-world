09c70ce5c7fd321c5a812d1e45cfa6adf2e6bb9f
EC2 Default User <ec2-user@ip-172-31-43-39.us-east-2.compute.internal>
2021-09-21 21:01:47 +0000

    sending task file

---
+++ task.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+mkdir newdir
+
+echo "Present working directory $PWD"
+
+cd newdir
+
+echo "changed path to newdir $PWD"
+
+touch shell.docx
+
+echo "create file $PWD"
+
+chmod 714 shell.docx
+
+
